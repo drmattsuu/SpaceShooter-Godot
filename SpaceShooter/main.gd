81760d334e631014497d1ec6d02d8aafc70aaf14
Matt Bennett <drmattsuu@users.noreply.github.com>
2024-02-04 09:38:17 +0000

    Add delay to game start

--- SpaceShooter/main.gd
+++ SpaceShooter/main.gd
@@ -1,4 +1,14 @@
 extends Node2D
 
+@export var baddie_scene: PackedScene
+
+func start_game():
+	var player = $Player
+	player.position = $SpawnPoint.position
+	player.show()
+
 func _ready():
-	$Player.position = $SpawnPoint.position
+	$Player.hide()
+	await get_tree().create_timer(3.0).timeout
+	start_game()
+
