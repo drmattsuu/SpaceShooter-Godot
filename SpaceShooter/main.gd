36d83a234245948cd2ccd8c6f5ddfbd68c7872a0
Matthew Bennett <3444149+drmattsuu@users.noreply.github.com>
2024-03-21 17:20:01 +0000

    add: start of UI work

--- SpaceShooter/main.gd
+++ SpaceShooter/main.gd
@@ -3,16 +3,37 @@ extends Node2D
 @export var baddie_scene: PackedScene
 
 var score = 0
+var highScore = 0
+
+func _update_label():
+    var scoreLabel = ($GameUI/ScoreValueLabel as RichTextLabel)
+    if scoreLabel:
+        scoreLabel.text = str(score)
+
+func incr_score(by: int):
+    score += by
+    _update_label()
+
+func set_score(s: int):
+    score = s
+    _update_label()
+
 
 func start_game():
     $Player.position = $SpawnPoint.position
     $Player.show()
-    score = 0
+    $StartUI.visible = false
+    $GameUI.visible = true
+    if score > highScore:
+        highScore = score
+    set_score(0)
     $BaddieTimer.start()
 
 func _ready():
     $Player.hide()
-    # await get_tree().create_timer(3.0).timeout
+    $GameUI.visible = false
+    $StartUI.visible = true
+    await get_tree().create_timer(3.0).timeout
     start_game()
 
 func _on_player_hit(body):
@@ -36,5 +57,4 @@ func _on_baddie_timer_timeout():
         $Baddies.add_child(newBaddie)
 
 func _baddie_killed():
-    score += 1
-    print(score)
+    incr_score(1)
